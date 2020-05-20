58ac1c53cce2cf105624d97cb94d2e0ecca42eb5
Ryan Gautier <ryangautier2@gmail.com>
2020-05-20 11:56:40 -0500

    style search form

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -4,12 +4,16 @@ import './style.css';
 
 function SearchForm() {
   return (
-  <div className="search-form">
-    <div className="form-group">
-    <label for="search-input">Search</label>
-    <input type="email" class="form-control" id="search-input" aria-describedby="search-input"></input>
+    <div className="search-form">
+      <h3>Book Search</h3>
+      <form>
+        <div className="form-group">
+          <label for="search-input">Book</label>
+          <input type="email" class="form-control" id="search-input" aria-describedby="search-input"></input>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
-  </div>
   )
 }
 
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { Col, Row, Container } from "../components/Grid";
-import { List, ListItem } from "../components/List";
-import { Input, TextArea, FormBtn } from "../components/Form";
 import SearchForm from '../components/SearchForm';
 
 
