54f817b751686a6ba3c8e1b8b0fb020eb48d1c9b
Ryan Gautier <ryangautier2@gmail.com>
2020-05-20 13:20:26 -0500

    use props in search result

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -8,10 +8,10 @@ function SearchForm() {
       <h3>Book Search</h3>
       <form>
         <div className="form-group">
-          <label for="search-input">Book</label>
-          <input type="email" class="form-control" id="search-input" aria-describedby="search-input"></input>
+          <label htmlFor="search-input">Book</label>
+          <input type="email" className="form-control" id="search-input" aria-describedby="search-input"></input>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     </div>
   )
--- client/src/components/SearchResult/index.js
+++ client/src/components/SearchResult/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './style.css';
 
-function SearchResult() {
+function SearchResult(props) {
   return (
     <div className="search-result">
-      <button type="button" class="float-right btn btn-primary">View</button>
-      <button type="button" class="float-right btn btn-outline-primary mr-2">Save</button>
-      <h4>Title</h4>
-      <h5>Author</h5>
+      <button type="button" className="float-right btn btn-primary">View</button>
+      <button type="button" className="float-right btn btn-info mr-2">Save</button>
+      <h4>{props.title}</h4>
+      <h5>{props.author}</h5>
       <div className="mt-4 mb-2">
         <div className="row no-gutters">
           <div className="col-md-4">
-            <img src="https://via.placeholder.com/300x200" className="card-img" alt="alt" />
+            <img src={props.image} className="card-img" alt={props.title} />
           </div>
           <div className="col-md-8">
             <div className="card-body">
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p className="card-text">{props.info}</p>
             </div>
           </div>
         </div>
